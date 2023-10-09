4b190a46c1dd32fc90e0d36cb8126e3e9e35396f
8URNT <81708706+8URNT@users.noreply.github.com>
2023-10-09 02:16:27 -0700

    Add files via upload

--- hypr/scripts/brightness.sh
+++ hypr/scripts/brightness.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the brightness scale as integer values
-brightness_scale=("0%" "1%" "5%" "10%" "15%" "20%" "25%" "30%" "35%" "40%" "45%" "50%" "55%" "60%" "65%" "70%" "75%" "80%" "85%" "90%" "95%" "100%")
+brightness_scale=("0%" "1%" "3%" "5%" "10%" "15%" "20%" "25%" "30%" "35%" "40%" "45%" "50%" "55%" "60%" "65%" "70%" "75%" "80%" "85%" "90%" "95%" "100%")
 
 get_current_brightness() {
     current_value=$(brightnessctl g)
--- hypr/scripts/suspend.sh
+++ hypr/scripts/suspend.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 swayidle -w \
 timeout 286 'systemctl suspend ' \
-before-sleep 'gtklock'
+before-sleep 'gtklock --time-format %I:%M'
